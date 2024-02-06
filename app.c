e4cf2dae6ecb4787e19bcd44a02e112b2b4e6872
SimSimSim <leivaprogramming@gmail.com>
2024-02-06 08:53:27 -0500

    git practice

---
+++ app.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    int x;
+    x = 9;
+
+    int y = 321;
+
+    bool e = true;
+
+    int age = 31;
+    float gpa = 2.05;
+    char grade = 'A';
+    char name[] = "Simon";
+
+    printf("You are %d years old",age);
+    printf("hello %s",name);
+    return 0;
+}
