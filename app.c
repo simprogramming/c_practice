2f1b7bac736d1c490c95311afb134d7e8d938907
SimSimSim <leivaprogramming@gmail.com>
2024-02-11 15:51:00 -0500

    c practice

--- app.c
+++ app.c
@@ -15,6 +15,12 @@ int main(){
     char grade = 'A';
     char name[] = "Simon";
 
+    int j = 214783647;
+    unsigned int k = 4294967295;
+
+    printf("%d\n", j);
+    printf("%u\n", k);
+
     printf("You are %d years old",age);
     printf("hello %s",name);
     return 0;
