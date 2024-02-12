71b41d444c22bc9058af7902eeb4b8ad49251f55
SimSimSim <leivaprogramming@gmail.com>
2024-02-12 12:40:44 -0500

    c practice

--- app.c
+++ app.c
@@ -2,22 +2,21 @@
 #include <stdbool.h>
 
 int main(){
-    // Allo
-    // format specifier % = defines and formats a type of data to be displayed
 
-    // %c = character
-    // %s = string (array of characters)
-    // %f = float
-    // %lf = double
-    // %d = integer
+    char name[25];
+    int age;
 
-    float item1 = 5.75;
-    float item2 = 10.00;
-    float item3 = 100.99;
+    printf("\nWhats your name?");
+    // scanf("%s", name);
+    fgets(name, 25, stdin);
 
-    printf("Item 1: $%.2f\n", item1);
-    printf("Item 2: $%.2f\n", item2);
-    printf("Item 3: $%.2f\n", item3);
+
+    printf("\nHow old are you?");
+    scanf("%d", &age);
+
+    printf("\nHello %s, how are you?", name);
+    printf("\nYou are %d years old\n", age);
+
 
     return 0;
 }
