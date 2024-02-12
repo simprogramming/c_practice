2b91857e47a91019a39890fb75b81bf066f302f1
SimSimSim <leivaprogramming@gmail.com>
2024-02-12 12:17:54 -0500

    c practice

--- app.c
+++ app.c
@@ -3,14 +3,21 @@
 
 int main(){
     // Allo
-    int age = 21;
-    float gpa = 2.05;
-    char grade = 'A';
-    char name[] = "Simon";
+    // format specifier % = defines and formats a type of data to be displayed
 
-    printf("Hello %s\n", name);
-    printf("You are %d years old\n", age);
-    printf("Your average grade is %c\n", grade);
-    printf("Your gpa is %f\n", gpa);
+    // %c = character
+    // %s = string (array of characters)
+    // %f = float
+    // %lf = double
+    // %d = integer
+
+    float item1 = 5.75;
+    float item2 = 10.00;
+    float item3 = 100.99;
+
+    printf("Item 1: $%.2f\n", item1);
+    printf("Item 2: $%.2f\n", item2);
+    printf("Item 3: $%.2f\n", item3);
+
     return 0;
 }
